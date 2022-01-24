e9b20a4a7571d03f27f5bc1150ecf974db729a2c
cjk28 <37836054+cjk28@users.noreply.github.com>
2022-01-24 18:53:56 +0000

    Update Natural_Mod.lua
    fix unnecessary brightness adjustment

--- Releases/Filters/Natural Mod Filter 7.3/MODS/Natural_Mod Filters/system/cfg/ppfilters/pure_scripts/Natural_Mod.lua
+++ Releases/Filters/Natural Mod Filter 7.3/MODS/Natural_Mod Filters/system/cfg/ppfilters/pure_scripts/Natural_Mod.lua
@@ -1,6 +1,3 @@
-local _l_brightness_filter = ac.ColorCorrectionHsb { hue=0, saturation=1.0, brightness=1.0, keepLuminance=true }
-ac.weatherColorCorrections[#ac.weatherColorCorrections + 1] = _l_brightness_filter
-
 function init_pure_script()
 	__SCRIPT__setVersion(7.4)
 	__SCRIPT__UI_SliderFloat("Contrast Day", 0.975, 0.96, 1.0)
@@ -17,11 +14,10 @@ function update_pure_script(dt)
 	--ac.setPpContrast(1 - ((1-contrast_day) * sun_compensate(0)))
 	ac.setPpContrast(1 - ((1-contrast_day) * day_compensate(0)))
 	ac.setPpTonemapGamma(1 + (gamma_day - 1	) * from_twilight_compensate(0))	
-	PURE__set_PP_Tonemapping_Curve(1)
+	ac.setPpTonemapMappingFactor(1)--try with 2
 	ac.setGlareBloomLuminanceGamma(0.85 + (0.75 * from_twilight_compensate(0)))
 	local ref_point = 1 / exp * day_compensate(0.65)
 	ac.setWhiteReferencePoint(ref_point)
 	ac.debug("contrast", string.format('%.3f', ac.getPpContrast()))
 	ac.debug("gamma", string.format('%.3f', ac.getPpTonemapGamma()))
-	_l_brightness_filter.brightness = 1.05 - (0.9 * exp)
 end
