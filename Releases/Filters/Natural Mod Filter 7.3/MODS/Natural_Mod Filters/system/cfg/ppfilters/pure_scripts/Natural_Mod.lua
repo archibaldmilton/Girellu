2dc58318edba48857cafd5680ad3043f89259d8f
cjk28 <37836054+cjk28@users.noreply.github.com>
2022-02-09 21:43:33 +0000

    Add files via upload

--- Releases/Filters/Natural Mod Filter 7.3/MODS/Natural_Mod Filters/system/cfg/ppfilters/pure_scripts/Natural_Mod.lua
+++ Releases/Filters/Natural Mod Filter 7.3/MODS/Natural_Mod Filters/system/cfg/ppfilters/pure_scripts/Natural_Mod.lua
@@ -1,7 +1,7 @@
 function init_pure_script()
 	__SCRIPT__setVersion(7.4)
 	__SCRIPT__UI_SliderFloat("Contrast Day", 0.975, 0.96, 1.0)
-	__SCRIPT__UI_SliderFloat("Gamma Day", 1.58, 1, 1.8)
+	__SCRIPT__UI_SliderFloat("Gamma Day", 1.58, 1.4, 1.7)
 	PURE__ExpCalc_set_Limits(.07,.5)
 	PURE__use_SpectrumAdaption(true)
 	PURE__use_VAOAdaption(true)
@@ -13,17 +13,17 @@ function init_pure_script()
 end
 
 function update_pure_script(dt)
-    ac.setGodraysLength(__IntD(8,4,0.6) * PURE__getGodraysModulator())
 	curr_ae = PURE__ExpCalc_get_final_exposure()
-	PURE__ExpCalc_set_Sensitivity(1.5 + -1 * (from_twilight_compensate(0.5)))
-	--PURE__ExpCalc_set_Target(0.3 + (from_twilight_compensate(0.7)))
-	ac.setPpSaturation(-0.025 + day_compensate(1.025))
-	contrast_day = __SCRIPT__UI_getValue("Contrast Day")
+	curve = from_twilight_compensate(2) - 1
 	gamma_day = __SCRIPT__UI_getValue("Gamma Day")
+	contrast_day = __SCRIPT__UI_getValue("Contrast Day")
+	PURE__ExpCalc_set_Sensitivity(1.5 + -1 * (from_twilight_compensate(0.5)))
+    ac.setGodraysLength(__IntD(8,4,0.6) * PURE__getGodraysModulator())
 	ac.setPpContrast(1 - ((1-contrast_day) * day_compensate(0)))
-	--ac.setPpTonemapGamma(1 + (gamma_day - 1) * from_twilight_compensate(0))--need exposure/brightness boost at night if want gamma at 1
-	ac.setPpTonemapGamma(gamma_day)	
+	ac.setPpTonemapGamma(gamma_day - (0.18 * curve))
+	ac.setPpBrightness(1 + (0.25 * curve))
 	ac.setPpTonemapFilmicContrast(0.25 + (0.25 * night_compensate(0)))
+	ac.setPpSaturation(-0.025 + day_compensate(1.025))
 	ac.setGlareBloomLuminanceGamma(math.lerp(1.7,1.1,curr_ae*2))
 	ac.debug("exp", string.format('%.3f', curr_ae))
 end
