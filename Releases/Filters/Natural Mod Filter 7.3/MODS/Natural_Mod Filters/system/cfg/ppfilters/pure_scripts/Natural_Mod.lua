90fadba0dc94b7189a26b487a41295b316c65991
cjk28 <37836054+cjk28@users.noreply.github.com>
2022-04-21 15:17:29 +0100

    Add files via upload

--- Releases/Filters/Natural Mod Filter 7.3/MODS/Natural_Mod Filters/system/cfg/ppfilters/pure_scripts/Natural_Mod.lua
+++ Releases/Filters/Natural Mod Filter 7.3/MODS/Natural_Mod Filters/system/cfg/ppfilters/pure_scripts/Natural_Mod.lua
@@ -12,11 +12,11 @@ end
 local tmp = 2
 local last_ae = 2
 local curr_ae = 2
-local overcast = 0
+local overcast
 local low_light_adaption_time  = 2
 local high_light_adaption_time = 0.25 --bigger = slower reaction
 local exp
-local gamma_min = 1.45
+local gamma_min
 local gamma_max = 1.6
 local contrast_min = 0.975
 local contrast_max = 0.985
@@ -37,20 +37,20 @@ function update_pure_script(dt)
         last_ae = math.lerp(last_ae, curr_ae, 1/low_light_adaption_time*math.min(1, dt))
     end
 
-	cbe_scaler = math.min(math.max(0,last_ae/(4.2*day_compensate(0.1))),1)--configure as 0 to 1 range, and disable at night -- replace with intD or similar so you dont get harsh transition at -3 to -12 sunangle
+	cbe_scaler = math.min(math.max(0,last_ae/(4.2*day_compensate(0.1))),1)-- replace with intD or similar so you dont get harsh transition at -3 to -12 sunangle?
 
-	contrast_min = math.max(math.min(__IntD(1,0.93,0.95),0.975),0.955)--outer values are the desired range, inner are wider to give more control
+	contrast_min = math.max(math.min(__IntD(1,0.93,0.95),0.985),0.975)--outer values are the desired range, inner are wider to give more control
 	gamma_min = math.max(math.min(__IntD(1.8,1.2,0.95),1.45),1.2)--outer values are the desired range, inner are wider to give more control
-	ac.setPpContrast(night_compensate(math.min(math.lerp(0.99,contrast_min,cbe_scaler)),contrast_max))-- higher value than actual max used to widen curve
+	ac.setPpContrast(night_compensate(math.min(math.lerp(1,contrast_min,cbe_scaler)),contrast_max))-- higher value than actual max used to widen curve
 	ac.setPpTonemapGamma(math.min(math.lerp(1.7,gamma_min,night_compensate(cbe_scaler)),gamma_max))-- higher value than actual max used to widen curve
-	ac.setPpTonemapFilmicContrast(0.15 + (0.25 * night_compensate(0)))--try 0.3
+	ac.setPpTonemapFilmicContrast(0.15 + (0.25 * night_compensate(0)))--review
 	exp = 0.1 + (0.05 * overcast) + ((0.35 + (0.15 - (cbe_scaler * 0.15))) * night_compensate(0))--ideally overcast addition would be smoothed
 	ac.setPpTonemapExposure(exp)
 
-	night_compensate_alt = sun_compensate(2) - 1
+	--night_compensate_alt = sun_compensate(2) - 1
 	ac.setPpSaturation(-0.025 + day_compensate(1.025))
 	ac.setGlareBloomLuminanceGamma(math.lerp(1.7,1.1,exp*2))
-	ac.setGodraysLength(__IntD(5.5,-2,0.4) * PURE__getGodraysModulator())--MAYBE TOO LOW?
+	ac.setGodraysLength(__IntD(5.5,-2,0.4) * PURE__getGodraysModulator())--consider the angle adjustment code
 
 	ac.debug("gamma", string.format('%.3f', ac.getPpTonemapGamma()))
 	ac.debug("contrast", string.format('%.3f', ac.getPpContrast()))
@@ -59,7 +59,6 @@ function update_pure_script(dt)
 	ac.debug("exp", string.format('%.3f', ac.getPpTonemapExposure()))
 	ac.debug("bright", string.format('%.3f', ac.getPpBrightness()))
 	ac.debug("1 cbe_scaler", string.format('%.3f', cbe_scaler))
-	ac.debug("1 overcast", string.format('%.3f', overcast))
 	ac.debug("1 curr_ae", string.format('%.3f', curr_ae))
 
 --copied from pure weather code:
