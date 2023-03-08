d9c03a272c8abe5f77ce76afd5b02304fa2ff744
cjk28 <37836054+cjk28@users.noreply.github.com>
2023-03-08 18:35:03 +0000

    Add files via upload

--- Releases/Filters/Natural Mod Filter 15/MODS/Natural_Mod Filters/system/cfg/ppfilters/pure_scripts/Natural_Mod.lua
+++ Releases/Filters/Natural Mod Filter 15/MODS/Natural_Mod Filters/system/cfg/ppfilters/pure_scripts/Natural_Mod.lua
@@ -4,35 +4,27 @@ local _l_ambient_LUT = {
     { 2,  1 },
     { 5,  0.9},
     { 6,  0.9 },
-    { 9,  0.2},
-    { 17, 0 },
+    { 17, 0.2 },
+    { 18, 0.0 },
     { 23, 0 },
 }
 local _l_Ambient_lut
 local _l_AmbientCPP = LUT:new(_l_ambient_LUT)
 
 function init_pure_script()
-	pure.script.tools.handleExposure(1, { autoexposure = 0.0 })--fixedmixmulti,fixedexposure
+	pure.script.tools.handleExposure(1, { autoexposure = 0.0 })
     pure.exposure.cbe.setAdaptionSpeeds(0.7, 1.5)
 	pure.exposure.cbe.setLimits(0.001, 0.25)
 	pure.light.setSpectrumAdaption(0.3)
 	pure.light.setVAOAdaption(0.8)
-	pure.script.weather.addVariable("cloud_brightness", 0)
-	pure.script.weather.setVariable("cloud_brightness", "FewClouds", 0.27)
-	pure.script.weather.setVariable("cloud_brightness", "ScatteredClouds", 0.22)
-	pure.script.weather.setVariable("cloud_brightness", "Windy", 0.25)
-	pure.script.weather.setVariable("cloud_brightness", "BrokenClouds", 0.2)
-	pure.script.weather.setVariable("cloud_brightness", "Haze", 0.4)
-	pure.script.weather.setVariable("cloud_brightness", "Mist", 0.5)
-	pure.script.weather.setVariable("cloud_brightness", "Thunderstorm", -0.3)
+	pure.light.setLambertGamma(1.7)
+	pure.config.set("clouds2D.brightness",0.85,true)
 	ac.setPpContrast(1)
 	ac.setPpSaturation(1)
 	ac.setGodraysGlareRatio(0)
-	ac.setGodraysDepthMapThreshold(0.999963)--tbd
 end
 
 local pureAE
-local cloud_bright
 local dark_ambient
 local cloud_cover
 local sun_lamp_diff
@@ -42,23 +34,20 @@ function update_pure_script(dt)
 	occlusion = pure.camera.getOcclusion()
 	cloud_cover = pure.world.getCloudCoverage()	
 	sun_lamp_diff = pure.light.getSunLampDifference()
-	cloud_bright = pure.script.weather.getVariable("cloud_brightness")
-	pure.config.set("clouds2D.brightness",1 - (pure.mod.twilight(0) * cloud_bright),true)
 	pure.config.set("light.daylight_multiplier",2.1,true)
-	pure.config.set("light.sun.saturation", pure.mod.dayCurve(1.10,1.40,1.5), true)
-	pure.light.setLambertGamma(math.clamp(__IntD(2.8,0.5,0.6),1,2.2))
-	pure.script.tools.handleGodrays(pure.mod.dayCurve(3,0.2,0.4),1,30,0.01,1.5)
+	pure.config.set("light.sun.saturation",0.9+0.4*(pure.mod.sun(2)-1), true)
+	pure.script.tools.handleGodrays(pure.mod.dayCurve(2.6,0.2,0.4),1,30,0.01,1.5)
 
 	_l_Ambient_lut  = _l_AmbientCPP:get(pure.light.getAmbientLuminance()/sun_lamp_diff)
 	dark_ambient = math.saturate(_l_Ambient_lut[1] * cloud_cover) * pure.mod.twilight(0)
 	
-	pure.exposure.cbe.setSensitivity(pure.mod.night(0.4))--note that AE no longer responds to changing daylight mult
-	pure.exposure.cbe.setTarget(1+dark_ambient*occlusion+0.5*pure.mod.night(0))
+	pure.exposure.cbe.setSensitivity(pure.mod.night(0.5))--note that AE no longer responds to changing daylight mult
+	pure.exposure.cbe.setTarget(0.99+dark_ambient*occlusion+0.5*pure.mod.night(0))
 	pureAE = pure.exposure.getCalculatedValue()
 	ac.setPpTonemapGamma(pure.pp.getGammaModulator() * (1.26 + (pure.mod.sun(2) -1)*0.31))
-	ac.setPpTonemapFilmicContrast(math.clamp(pure.mod.dayCurve(0.55, 0, 0.5),0.18,0.5))--+0.1*dark_ambient
+	ac.setPpTonemapFilmicContrast(math.clamp(math.max(pure.mod.dayCurve(0.55, 0, 0.5),0.6*dark_ambient),0.18,0.5))
 
     pure.config.set("csp_lights.bounce", 1.6, true)	
-	pure.config.set("csp_lights.emissive",2/(pure.exposure.cbe.getValue()*10), true)
+	pure.config.set("csp_lights.emissive",0.4 + 2/(pure.exposure.cbe.getValue()*10), true)
 	pure.config.set("reflections.level", 1.1 + math.clamp(pureAE*10-0.5,0,0.3), true)
-end
+end	
--- Releases/Filters/Natural Mod Filter 15/MODS/Natural_Mod Filters/system/cfg/ppfilters/sol_custom_configs/Natural_Mod.lua
+++ Releases/Filters/Natural Mod Filter 15/MODS/Natural_Mod Filters/system/cfg/ppfilters/sol_custom_configs/Natural_Mod.lua
@@ -8,7 +8,7 @@ function init_sol_custom_config()
 	SOL__set_config("moon", "casts_shadows", false)
 	SOL__set_config("nerd__csp_lights_adjust", "emissive_day", 1.0)
 	ac.setGodraysAngleAttenuation(30)
-    ac.setGodraysCustomColor(rgb(10,10,10))
+    ac.setGodraysCustomColor(rgb(20,20,20))
 	ac.setPpContrast(1)
 	ac.setPpSaturation(1)
 end
@@ -17,7 +17,7 @@ function update_sol_custom_config(dt)
 	ac.setPpTonemapExposure(0.36 + (0.09 *duskdawn_compensate(0)))
 	ac.setPpTonemapGamma(gamma_base - (math.lerp(0, 0.15, weather__get_overcast()) * day_compensate(0)))--base - 0.15 if overcast daytime
 	ac.setPpTonemapFilmicContrast(0.25 + (0.15 * night_compensate(0)))
-	ac.setGodraysLength(__IntD(5,-2,0.4) * day_compensate(0))
+	ac.setGodraysLength(__IntD(7,-2,0.4) * day_compensate(0))
 	SOL__set_config("pp", "brightness_sun_link", (0.3 + (0.7 * night_compensate(0))))--0.3 day 1.0 night
 	SOL__set_config("night", "brightness_adjust", 0.1, true)
 	SOL__set_config("night", "moonlight_multiplier", 1.5, true)
